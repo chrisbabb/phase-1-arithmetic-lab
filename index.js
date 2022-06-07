9d5f3be8cf4de416213c2b26cf056c0de1d83062
Christopher Babb <chris@appsoftdev.com>
2022-06-07 10:07:18 -0700

    commit tree 497eb7f7b3d87413803207b5f89e09df2b4e3b26

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
 // Write your code here
+const num1 = 2
+const num2 = 31
+const num3 = 16
+const num4 = 4
+let multiply = 2 * 31
+let random = Math.floor(Math.random() * 10) + 1
+let mod = 16 / 4
+let max = Math.max(10,4,3,6,20)
